2751a536ea4598249ed4b3c7d3b2d3d1d883f919
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-23 16:51:55 +0100

    creato componente CardReview

---
+++ src/components/CardReview.jsx
@@ -0,0 +1,16 @@
+function CardReview() {
+
+    return (
+        <div className="card mb-4">
+            <div className="card-body">
+                <p className="card-text">
+                    text review
+                </p>
+                <strong>Vote: 5</strong>
+                <address><i>author's review</i></address>
+            </div>
+        </div>
+    )
+}
+
+export default CardReview
