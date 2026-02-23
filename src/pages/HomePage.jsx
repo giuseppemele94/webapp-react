398254b585a7cca76bfe2027a51848ec6af7d11f
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-23 15:57:12 +0100

    create simple page per HomePage e pagina del dettaglio film

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+function HomePage() {
+    return(
+        <>
+          <h1 className="text-primary">Movies</h1>
+            <h2><i>The community of Movies</i></h2>
+
+            <div>
+                <p className="row row-cols-3 mt-4">
+                    Elenco dei film
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,9 @@
+function MoviePage () {
+    return(
+        <>
+        <h1>Dettaglio del film</h1>
+        </>
+    )
+}
+
+export default MoviePage
