92c53721e92cdda68dc48fbc7a23f67eb915108b
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-23 16:56:55 +0100

    import componente CardReview in MoviePage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,19 +1,31 @@
 import { Link } from "react-router-dom"
 
-
+//import componente cardReview
+import CardReview from "../components/CardReview"
 
 function MoviePage() {
     return (
         <>
             <header id="movie " className="border-bottom border-1 mb-3">
                 <div className="d-flex mb-3 ">
-                    <img src="https://www.movietele.it/wp-content/uploads/2021/08/281912-be0.jpg" alt="titolo del film"  
-                    className="img-fluid movie-poster"/>
+                    <img src="https://www.movietele.it/wp-content/uploads/2021/08/281912-be0.jpg" alt="titolo del film"
+                        className="img-fluid movie-poster" />
                 </div>
                 <h1>Titolo film</h1>
                 <h3 className="text-muted"><i>Director</i></h3>
                 <p>lorem ipsm dolor sit amet</p>
             </header>
+
+            <section id="reviews">
+                <header className="d-flex justify-content-between align-items-center mb-4">
+                    <h4>Our community reviews</h4>
+                </header>
+
+                <CardReview />
+                <CardReview />
+
+            </section>
+
             <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
                 <Link className="btn btn-secondary" to="/">Back to home</Link>
             </footer>
