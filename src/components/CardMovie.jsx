2bf6ecc168f31b07101d362958adccb4073773f2
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-23 16:28:33 +0100

    layout base componente CardMovie e immportato in HomePAge

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
 
-function CardMovie () {
+function CardMovie() {
 
     return (
+        <div className="card mb-4" >
+            <img src="..."
+                className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h3 className="card-title">Card title</h3>
+                <h5 className="card-subtitle mb-2 text-body-secondary">Director</h5>
+                <h6 className="card-subtitle mb-2 text-body-secondary">Genre</h6>
+                <p className ="card-text"><i>Year</i></p>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <address><i>Created at</i></address>
+                <address><i>Updated at</i></address>
+                <Link to={`movies/1`} className="btn btn-primary">Read More</Link>
+            </div>
+        </div>
 
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,21 @@
+import CardMovie from "../components/CardMovie"
+
 function HomePage() {
-    return(
+    return (
         <>
-          <h1 className="text-primary">Movies</h1>
+            <h1 className="text-primary">Movies</h1>
             <h2><i>The community of Movies</i></h2>
 
             <div>
-                <p className="row row-cols-3 mt-4">
-                    Elenco dei film
-                </p>
+                <div className="row row-cols-3 mt-4">
+                    <CardMovie />
+                    <CardMovie />
+                    <CardMovie />
+                    <CardMovie />
+                    <CardMovie />
+                    <CardMovie />
+
+                </div>
             </div>
         </>
     )
