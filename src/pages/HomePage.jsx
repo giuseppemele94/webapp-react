a07b12c134589edcb625e8b7b96d1b63e1b4fb96
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-24 16:05:26 +0100

    chiamata AJAX rotta INDEX al listato dei film in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,37 @@
 import CardMovie from "../components/CardMovie"
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+const endpoint = "http://localhost:3000/api/movies";
 
 function HomePage() {
+
+    //impost variabile di stato 
+    const [movies, setMovies] = useState([]);
+
+    //chiamata AJAX alla rotta INDEX del BACKEND piu UseEffect al montaggio della pagina 
+    useEffect(() => {
+        axios.get(endpoint)
+            .then(res => {
+                console.log(res.data)
+                setMovies(res.data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
+
+    //funzione di rendering della lista dei film 
+    const renderMovies = () => {
+        return movies.map(movie => {
+            return (
+                <div className="col" key={movie.id}>
+                    <CardMovie movie={movie} />
+                </div>
+            )
+        })
+    }
+
     return (
         <>
             <h1 className="text-primary">Movies</h1>
@@ -8,12 +39,7 @@ function HomePage() {
 
             <div>
                 <div className="row row-cols-3 mt-4">
-                    <CardMovie />
-                    <CardMovie />
-                    <CardMovie />
-                    <CardMovie />
-                    <CardMovie />
-                    <CardMovie />
+                    {renderMovies()}
 
                 </div>
             </div>
