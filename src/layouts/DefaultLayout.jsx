396127b583970e60179f17d7ef6a81fe6bb8f0e1
Giuseppe Mele <giuseppemele9410@gmail.com>
2026-02-26 15:02:20 +0100

    creato context globale + test funzionamento

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,6 +2,11 @@ import { Outlet } from "react-router-dom";
 
 import MainHeader from "../components/MainHeader";
 
+import Loader from "../components/Loader";
+
+//variabile di settaggio caricamento
+const isLoading = true ; 
+
 function DefaultLayout() {
     
     return(
@@ -10,6 +15,7 @@ function DefaultLayout() {
         <main className="container">
             <Outlet/>
         </main>
+        {isLoading && <Loader/>}
         </>
     )
 }
